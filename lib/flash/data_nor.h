3bf6764ead1961aaec72ac95cf8dac738c591074
Fumito Morishima <fumito@spacecubics.com>
2025-06-05 14:51:07 +0900

    lib/flash: Define constants for telemetry flash storage layout
     Added macros to define block size, total flash size, start address,
     erase sector size, and maximum number of telemetry entries.
    
    Signed-off-by: Fumito Morishima <fumito@spacecubics.com>

--- lib/flash/data_nor.h
+++ lib/flash/data_nor.h
@@ -6,6 +6,12 @@
 
 #pragma once
 
+#define TLM_BLOCK_SIZE 256
+#define TLM_FLASH_SIZE (4 * 1024 * 1024) // 4MiB
+#define TLM_START_ADDR 0x00000000
+#define ERASE_SECTOR_SIZE 4096
+#define MAX_TLM_STORE_NUM (TLM_FLASH_SIZE/TLM_BLOCK_SIZE)
+
 int datafs_init(void);
 int update_boot_count(const char *fname);
 int data_nor_erase(uint8_t id);
