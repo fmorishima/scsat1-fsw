5bfaa5493602cac693ad5367d48866b5b88c2ed2
Fumito Morishima <fumito@spacecubics.com>
2025-12-29 16:51:56 +0900

    main/src/tlm: implement syshk storage and retrieval functions
     - Synchronize the current sequence number with FRAM during the
       telemetry task.
     - Add store_syshk() to save telemetry entries to flash with sector
       erase handling.
     - Ensure telemetry is stored even during the downlink inhibit
       period.
     - Implement send_syshk_history_to_ground() to read and transmit
       data from NOR flash.
     - Add request parameter validation (range and interval checks).
     - Implement a flag-based cancellation mechanism to interrupt
       long-running downlinks.
    
    Signed-off-by: Fumito Morishima <fumito@spacecubics.com>

--- main/src/tlm/syshk.c
+++ main/src/tlm/syshk.c
@@ -3,10 +3,15 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/flash.h>
+
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/zbus/zbus.h>
 #include <csp/drivers/can_zephyr.h>
 #include <csp/csp.h>
+#include "data_nor.h"
 #include "sc_csp.h"
 #include "syshk.h"
 #include "system.h"
@@ -14,6 +19,7 @@
 #include "cv.h"
 #include "mgnm_mon.h"
 #include "sunsens_mon.h"
+#include "fram.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(syshk, CONFIG_SCSAT1_MAIN_LOG_LEVEL);
@@ -193,27 +199,27 @@ static void syshk_sub_task(void *sub)
 	while (!zbus_sub_wait(subscriber, &chan, K_FOREVER)) {
 		if (&system_chan == chan) {
 			zbus_chan_read(chan, &system_msg,
-				       K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
+					   K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
 			LOG_DBG("Subscribe SYSTEM msg %d byte", sizeof(system_msg));
 			copy_system_to_syshk(&system_msg);
 		} else if (&temp_chan == chan) {
 			zbus_chan_read(chan, &temp_msg,
-				       K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
+					   K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
 			LOG_DBG("Subscribe TEMP msg %d byte", sizeof(temp_msg));
 			copy_temp_to_syshk(&temp_msg);
 		} else if (&cv_chan == chan) {
 			zbus_chan_read(chan, &cv_msg,
-				       K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
+					   K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
 			LOG_DBG("Subscribe CV msg %d byte", sizeof(cv_msg));
 			copy_cv_to_syshk(&cv_msg);
 		} else if (&mgnm_chan == chan) {
 			zbus_chan_read(chan, &mgnm_msg,
-				       K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
+					   K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
 			LOG_DBG("Subscribe MGNM msg %d byte", sizeof(mgnm_msg));
 			copy_mgnm_to_syshk(&mgnm_msg);
 		} else if (&sunsens_chan == chan) {
 			zbus_chan_read(chan, &sunsens_msg,
-				       K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
+					   K_MSEC(CONFIG_SCSAT1_MAIN_ZBUS_READ_TIMEOUT_MSEC));
 			LOG_DBG("Subscribe SUN msg %d byte", sizeof(sunsens_msg));
 			copy_sunsens_to_syshk(&sunsens_msg);
 		} else {
@@ -226,7 +232,7 @@ static void syshk_sub_task(void *sub)
 K_THREAD_DEFINE(syshk_sub_task_id, CONFIG_SCSAT1_MAIN_SUB_SYSHK_THREAD_STACK_SIZE, syshk_sub_task,
 		&syshk_sub, NULL, NULL, CONFIG_SCSAT1_MAIN_SUB_SYSHK_THREAD_PRIORITY, 0, 0);
 
-void send_syshk_to_ground(void)
+static void send_syshk_to_ground_impl(struct syshk_tlm *syshk_tlm)
 {
 	csp_conn_t *conn;
 	csp_packet_t *packet;
@@ -238,35 +244,195 @@ void send_syshk_to_ground(void)
 		goto end;
 	}
 
-	packet = csp_buffer_get(0);
+	packet = csp_buffer_get(CSP_BUFFER_SIZE);
 	if (packet == NULL) {
 		LOG_ERR("Failed to get CSP buffer");
 		goto close;
 	}
 
-	memcpy(&packet->data, &syshk, sizeof(syshk));
-	packet->length = sizeof(syshk);
+	memcpy(&packet->data, syshk_tlm, sizeof(struct syshk_tlm));
+	packet->length = sizeof(struct syshk_tlm);
 
 	csp_send(conn, packet);
-	LOG_DBG("Send HK to GND %d byte", packet->length);
-	syshk.seq_num++;
+	LOG_DBG("Send HK to GND %d byte, seq: %d", packet->length, syshk_tlm->seq_num);
 
+	csp_buffer_free(packet);
 close:
 	csp_close(conn);
 
 end:
 }
 
-static void send_syshk(struct k_work *work)
+void send_syshk_to_ground(void)
 {
-	ARG_UNUSED(work);
+	send_syshk_to_ground_impl(&syshk);
+}
 
-	if (IS_ENABLED(CONFIG_SCSAT1_MAIN_AUTO_SYSHK_DOWNLINK)) {
-		send_syshk_to_ground();
+static volatile bool cancel_syshk_history_flag = false;
+void cancel_syshk_history(void)
+{
+	LOG_INF("current history cancel flag: %d", cancel_syshk_history_flag);
+	cancel_syshk_history_flag = true;
+}
+
+enum history_req_err_code {
+	HISTORY_REQ_ERR_NONE = 0,
+	HISTORY_REQ_ERR_INVALID_INTERVAL,
+	HISTORY_REQ_ERR_INVALID_SEQ_NUM,
+	HISTORY_REQ_ERR_SEQ_NUM_OUT_OF_RANGE,
+};
+
+# define MIN_HISTORY_INTERVAL_MS 10
+# define MAX_HISTORY_INTERVAL_MS 1000
+int check_syshk_history_req_param(uint32_t start_seq_num, uint32_t end_seq_num, uint16_t send_intvl_ms)
+{
+	const uint32_t curr_seq_num = syshk.seq_num;
+
+	if (start_seq_num > end_seq_num) {
+		LOG_ERR("Invalid range, start: %u, end: %u", start_seq_num, end_seq_num);
+		return HISTORY_REQ_ERR_INVALID_SEQ_NUM;
+	}
+
+	if (send_intvl_ms < MIN_HISTORY_INTERVAL_MS || send_intvl_ms > MAX_HISTORY_INTERVAL_MS) {
+		LOG_ERR("Invalid interval: %u ms", send_intvl_ms);
+		return HISTORY_REQ_ERR_INVALID_INTERVAL;
+	}
+
+	if (end_seq_num >= curr_seq_num){ // at least one behind from the latest
+		LOG_ERR("Future seq requested. Req: %u, Curr: %u", end_seq_num, curr_seq_num);
+		return HISTORY_REQ_ERR_INVALID_SEQ_NUM;
+	}
+
+	uint32_t oldest_seq_num = (curr_seq_num > AVAILABLE_HISTORY_COUNT) ?
+				(curr_seq_num - AVAILABLE_HISTORY_COUNT): 0;
+
+	if (start_seq_num < oldest_seq_num){
+		LOG_ERR("Req seq too old. Req: %u, Oldest: %u", start_seq_num, oldest_seq_num);
+		return HISTORY_REQ_ERR_SEQ_NUM_OUT_OF_RANGE;
+	}
+
+	return HISTORY_REQ_ERR_NONE;
+}
+
+void send_syshk_history_to_ground(uint32_t start_seq_num, uint32_t end_seq_num, uint16_t skip_count , uint16_t send_intvl_ms)
+{
+	if (check_syshk_history_req_param(start_seq_num, end_seq_num, send_intvl_ms) != HISTORY_REQ_ERR_NONE){
+		return;
+	}
+
+	struct syshk_tlm syshk_tlm;
+	const struct device *flash_dev = stored_tlm_flash_dev();
+
+	if (!device_is_ready(flash_dev)) {
+		LOG_ERR("Flash device %s is not ready", flash_dev->name);
+		return;
+	}
+
+	cancel_syshk_history_flag = false;
+	uint32_t read_pos = start_seq_num % MAX_TLM_STORE_NUM;
+
+	LOG_INF("Start sending history tlm, start: %u, end: %u", start_seq_num, end_seq_num);
+
+	for (int i = start_seq_num; i <= end_seq_num; i += skip_count + 1){
+		uint32_t read_addr = read_pos * TLM_BLOCK_SIZE + TLM_START_ADDR;
+		int ret = flash_read(flash_dev, read_addr, &syshk_tlm, sizeof(syshk_tlm));
+		if (ret == 0){
+			send_syshk_to_ground_impl(&syshk_tlm);
+			k_sleep(K_MSEC(send_intvl_ms));
+		}
+		else{
+			LOG_ERR("Failed to read history syshk data from flash at address 0x%08X: %d",
+					read_addr, ret);
+		}
+
+		if (cancel_syshk_history_flag){
+			LOG_INF("History retrieval cancelled at seq: %u", i);
+			return;
+		}
+		read_pos = (read_pos + skip_count + 1) % MAX_TLM_STORE_NUM;
 	}
 }
 
-static K_WORK_DEFINE(syshk_work, send_syshk);
+static void store_syshk(struct syshk_tlm *syshk_tlm)
+{
+	const struct device* flash_dev = stored_tlm_flash_dev();
+	int ret;
+	uint32_t write_pos;
+	uint32_t write_addr;
+
+	if (!device_is_ready(flash_dev)) {
+		LOG_ERR("Flash device %s is not ready", flash_dev->name);
+		return;
+	}
+
+	write_pos = syshk_tlm->seq_num % MAX_TLM_STORE_NUM;
+	write_addr = write_pos * TLM_BLOCK_SIZE + TLM_START_ADDR;
+
+	if(write_addr % ERASE_SECTOR_SIZE == 0){
+		LOG_INF("reached to erase unit address 0x%08X, seq_num: %d\n",
+			write_addr, syshk_tlm->seq_num);
+		ret = flash_erase(flash_dev, write_addr, ERASE_SECTOR_SIZE);
+		if(ret != 0){
+			LOG_ERR("Failed to erase flash sector at address 0x%08X: %d",
+				write_addr, ret);
+			return;
+		}
+	}
+
+	if(!is_flash_erased(flash_dev, write_addr, TLM_BLOCK_SIZE)){
+		uint32_t sector_addr = write_addr & ~(ERASE_SECTOR_SIZE - 1);
+		LOG_INF("block is not erased, force erase at address 0x%08X, seq_num: %d\n",
+				sector_addr, syshk_tlm->seq_num);
+		ret = flash_erase(flash_dev, sector_addr, ERASE_SECTOR_SIZE);
+		if(ret != 0){
+			LOG_ERR("Failed to erase flash sector at address 0x%08X: %d",
+				sector_addr, ret);
+			return;
+		}
+	}
+
+	ret = flash_write(flash_dev, write_addr, syshk_tlm, sizeof(*syshk_tlm));
+	if(ret != 0){
+		LOG_ERR("Failed to write syshk_tlm to flash at address 0x%08X: %d",
+				write_addr, ret);
+		return;
+	}
+}
+
+static void handle_syshk(struct k_work *work)
+{
+	ARG_UNUSED(work);
+
+	uint32_t saved_seq_num;
+	if(sc_fram_get_tlm_seq_num(&saved_seq_num) >= 0){
+		if (syshk.seq_num == 0) { // the first time sending syshk after boot
+			syshk.seq_num = saved_seq_num;
+		}
+		else if (syshk.seq_num != saved_seq_num) {
+			LOG_ERR("Curr seq number differs from the saved value %d != %d",
+					syshk.seq_num, saved_seq_num);
+			syshk.seq_num = saved_seq_num;
+		}
+	}
+	else {
+		LOG_ERR("Failed to get saved seq num from FRAM, curr: %d", syshk.seq_num);
+	}
+
+	syshk.seq_num++;
+
+	store_syshk(&syshk);
+
+	if (IS_ENABLED(CONFIG_SCSAT1_MAIN_AUTO_SYSHK_DOWNLINK) &&
+		(k_uptime_get_32() / MSEC_PER_SEC) > CONFIG_SCSAT1_MAIN_SYSHK_INHIBIT_PERIOD_SEC){
+		send_syshk_to_ground();
+	}
+
+	if(sc_fram_update_tlm_seq_num() < 0){
+		LOG_ERR("Failed to update saved seq num, curr num : %d", syshk.seq_num);
+	}
+}
+
+static K_WORK_DEFINE(syshk_work, handle_syshk);
 
 static void syshk_handler(struct k_timer *dummy)
 {
@@ -282,6 +448,6 @@ void start_send_syshk(void)
 			   CONFIG_SCSAT1_MAIN_SEND_SYSHK_THREAD_PRIORITY, NULL);
 	k_thread_name_set(&syshk_workq.thread, "syshk_workq");
 
-	k_timer_start(&syshk_timer, K_SECONDS(CONFIG_SCSAT1_MAIN_SYSHK_INHIBIT_PERIOD_SEC),
-		      K_SECONDS(CONFIG_SCSAT1_MAIN_SYSHK_INTERVAL_SEC));
+	k_timer_start(&syshk_timer, K_SECONDS(CONFIG_SCSAT1_MAIN_SYSHK_INTERVAL_SEC),
+			  K_SECONDS(CONFIG_SCSAT1_MAIN_SYSHK_INTERVAL_SEC));
 }
